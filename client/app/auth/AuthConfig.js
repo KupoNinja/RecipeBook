8ea365d4a4a9a4f8e457e96ad6cac2808b102b66
JJ Garlitos <jj.garlitos@gmail.com>
2020-02-27 19:36:27 -0700

    Initial Commit

---
+++ client/app/auth/AuthConfig.js
@@ -0,0 +1,4 @@
+// TODO Add Auth0 Config
+export const domain = 'recipebook.auth0.com';
+export const clientId = 'bqjKNEvo5fYXPprUjqRPkIZik39enQC1';
+export const audience = 'https://recipebook-api.com';
